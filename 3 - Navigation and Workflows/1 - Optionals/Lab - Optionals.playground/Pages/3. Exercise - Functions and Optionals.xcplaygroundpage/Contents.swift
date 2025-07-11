ffd8c4ebf966d624a289f930bf61834b332ebbd6
ghanshyam0167 <ghanshyam0167.be23@chitkara.edu.in>
2025-07-11 10:01:25 +0530

    Optionals

--- 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
@@ -5,15 +5,47 @@
  */
 let userInputAge: String = "34e"
 
-
+func checkAge(stringValue : String) -> Int {
+    if let intValue = Int(stringValue) {
+        if intValue >= 18 {
+            print("Welcome!")
+            return intValue
+        }
+        else{
+            print("Sorry, but you aren't old enough to use our app")
+            return intValue
+        }
+    }
+    else{
+        print("Sorry, something went wrong. Can you please re-enter your age?")
+        return 0
+    }
+   
+    
+}
 //:  Go back and update your function to return the age as an integer. Will your function always return a value? Make sure your return type accurately reflects this. Call the function and print the return value.
+print(checkAge(stringValue: "42sd"))
 
 
 //:  Imagine you are creating an app for making purchases. Write a function that will take in the name of an item for purchase as a `String` and will return the cost of that item as an optional `Double`. In the body of the function, check to see if the item is in stock by accessing it in the dictionary `stock`. If it is, return the price of the item by accessing it in the dictionary `prices`. If the item is out of stock, return `nil`. Call the function and pass in a `String` that exists in the dictionaries below. Print the return value.
 var prices = ["Chips": 3.69, "Donuts": 1.89, "Juice": 4.99, "Apple": 0.70, "Banana": 0.53, "Broccoli": 1.99]
 var stock = ["Chips": 4, "Donuts": 0, "Juice": 12, "Apple": 6, "Banana": 6, "Broccoli": 3]
 
+func buySnack(item : String) -> Double? {
+    if let itemStock = stock[item] {
+        return prices[item]
+    }
+    else{
+        return nil
+    }
+}
 
+if let itemPrice = buySnack(item: "Chips") {
+    print("Price of item is \(itemPrice)")
+}
+else{
+    print("Item not found")
+}
 /*:
 [Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Food Functions](@next)
  */
--- 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/6. App Exercise - Workout or Nil.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/6. App Exercise - Workout or Nil.xcplaygroundpage/Contents.swift
@@ -9,11 +9,38 @@
 
  Write a failable initializer that takes parameters for your start and end times, and then checks to see if they are fewer than 10 seconds apart. If they are, your initializer should fail. Otherwise, they should set the properties accordingly.
  */
-
+struct Workout{
+    var startTime : Double
+    var endTime : Double
+    
+    init?(startTime: Double,endTime: Double ) {
+        if endTime - startTime < 10 {
+            return nil
+        }
+        self.startTime = startTime
+        self.endTime = endTime
+    }
+}
 
 //:  Try to initialize two instances of a `Workout` object. Unwrap each of them and print its properties. One of them should not be initialized because the start and end times are too close together. The other should successfully initialize a `Workout` object.
+let firstWorkout = Workout(startTime: 28800, endTime: 28880)
+let secondWorkout = Workout(startTime: 28800, endTime: 28805)
 
+if let firstWorkout = firstWorkout {
+    print(firstWorkout.startTime)
+    print(firstWorkout.endTime)
+}
+else{
+    print("Failed to create instance")
+}
 
+if let secondWorkout = secondWorkout {
+    print(secondWorkout.startTime)
+    print(secondWorkout.endTime)
+}
+else{
+    print("Failed to create instance")
+}
 /*:
  _Copyright © 2023 Apple Inc._
 
